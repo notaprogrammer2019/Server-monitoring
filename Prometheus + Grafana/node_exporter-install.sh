0610403d0ad59dc71de06c3f6992c4afba57596e
sadaman <47750617+notaprogrammer2019@users.noreply.github.com>
2022-05-28 19:36:46 +0700

    Update node_exporter-install.sh

--- Prometheus + Grafana/node_exporter-install.sh
+++ Prometheus + Grafana/node_exporter-install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz && tar xvf node_exporter-1.3.1.linux-amd64.tar.gz && rm node_exporter-1.3.1.linux-amd64.tar.gz && chmod +x $HOME/node_exporter-1.3.1.linux-amd64/node_exporter && mv $HOME/node_exporter-1.3.1.linux-amd64/node_exporter /usr/bin && rm -Rf $HOME/node_exporter-1.3.1.linux-amd64
+wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz && tar xvf node_exporter-1.3.1.linux-amd64.tar.gz && rm node_exporter-1.3.1.linux-amd64.tar.gz && chmod +x $HOME/node_exporter-1.3.1.linux-amd64/node_exporter && mv $HOME/node_exporter-1.3.1.linux-amd64/node_exporter /usr/bin && rm -Rf $HOME/node_exporter-1.3.1.linux-amd64 && printf "EOF
+[Unit]
+Description=node_exporter
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=/usr/bin/node_exporter
+Restart=always
+RestartSec=3
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF"  > /etc/systemd/system/node-exported.service && systemctl daemon-reload && systemctl enable node-exported.service && systemctl restart node-exported.service
